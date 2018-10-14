23a69080a9c84e74bf8a2e525e8c2820f1b52485
chriscauley <chris@lablackey.com>
2018-10-14 00:29:35 +0000

    moving riot into the load step to run it before babel

--- RiotAsset.js
+++ RiotAsset.js
@@ -3,15 +3,12 @@ const JSAsset = require('parcel-bundler/src/assets/JSAsset');
 const preamble = "const riot = require('riot');\n";
 
 class RiotAsset extends JSAsset {
-
-  async parse(inputCode) {
+  async load() {
+    const content = await super.load();
     const riotOpts = {};
 
-    let code = compile(inputCode, riotOpts, this.name);
-    code = `${ preamble }${ code }`;
-    this.contents = code;
-
-    return super.parse(this.contents);
+    let compiled = compile(content, riotOpts, this.name);
+    return `${ preamble }${ compiled }`;
   }
 }
 
